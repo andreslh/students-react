4078a68a8e0238cf22d234f90173446cb3c8913f
Andrés Ledesma Higonet <andreslh90@gmail.com>
2022-04-15 10:52:39 -0300

    feat(footer): back to top button checked by scroll

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Footer = () => {
+  const [goToTopVisible, setGoToTopVisible] = useState(false);
+  const [scrollTop, setScrollTop] = useState(0);
+
   const goToTop = (e) => {
     e.preventDefault();
     window.scrollTo(0, 0);
   };
 
+  useEffect(() => {
+    const onScroll = (e) => {
+      const documentScroll = e.target.documentElement.scrollTop;
+      setScrollTop(documentScroll);
+      setGoToTopVisible(documentScroll > scrollTop);
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
   return (
     <footer className="text-muted mt-5">
       <div className="container">
         <p className="float-right">
-          <a href="#" onClick={goToTop}>
-            Back to top
-          </a>
+          {goToTopVisible && (
+            <a href="#" onClick={goToTop}>
+              Back to top
+            </a>
+          )}
         </p>
       </div>
     </footer>
